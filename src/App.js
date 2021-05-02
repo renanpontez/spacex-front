6f5b22064994036a6e4a4b3c0d96493f0045b838
Renan Martins <renan@deonibus.com>
2021-05-02 16:43:25 -0300

    GA Tracking code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import { ROUTE_HOME } from './utils/routes';
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-G4GDE3LRWZ');
 
 function App() {
   return (
