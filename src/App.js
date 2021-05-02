8d3c3e115f2c4a1c35abd16b8ae35e8162d0fc02
Renan Martins <renan@deonibus.com>
2021-05-02 17:12:25 -0300

    adding optimize code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import { ROUTE_HOME } from './utils/routes';
 import ReactGA from 'react-ga';
-ReactGA.initialize('UA-16615881-8');
+import { hotjar } from 'react-hotjar';
+
+hotjar.initialize(
+  process.env.REACT_APP_HOTJAR_ID,
+  process.env.REACT_APP_HOTJAR_SNIPPET,
+);
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING);
 
 function App() {
   return (
